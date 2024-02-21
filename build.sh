96aace28ea25f74f5bf20d427d0a4d77a92872d4
vol <me@volodymyrstoiko.com>
2024-02-21 20:51:47 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 cd certs
 ./generate.sh
 cd ../nginx
-docker buildx build --platform linux/amd64 -t alongir/ks-load-server:latest . --push 
+docker buildx build --platform linux/amd64 -t kubehq/k6s-load-server:latest . --push
 cd ../k6
-docker buildx build --platform linux/amd64 -t alongir/k6-loadtest:latest . --push 
+docker buildx build --platform linux/amd64 -t kubehq/k6s-load-test:latest . --push
